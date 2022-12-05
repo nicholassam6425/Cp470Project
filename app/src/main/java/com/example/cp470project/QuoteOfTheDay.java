0b95abdc6f0ba36738b8679db646ab7c7291f593
Bench Appearo <joshuahaberly@gmail.com>
2022-12-05 18:59:06 -0500

    Fixes after merge

--- app/src/main/java/com/example/cp470project/QuoteOfTheDay.java
+++ app/src/main/java/com/example/cp470project/QuoteOfTheDay.java
@@ -3,69 +3,135 @@ package com.example.cp470project;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Calendar;
 
 public class QuoteOfTheDay extends AppCompatActivity {
     public int day = 1;
+
+    private class QuoteQuery extends AsyncTask<String, Integer, String> {
+        public String quote;
+        TextView textView = findViewById(R.id.quote);
+        ProgressBar bar = findViewById(R.id.progressBar);
+
+        QuoteQuery(Context ctx){
+
+        }
+        @Override
+        protected void onPostExecute(String a){
+            bar.setVisibility(View.INVISIBLE);
+            textView.setText(quote);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values){
+            bar.setProgress(values[0]);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try{
+                InputStream result = downloadUrl("https://www.zenquotes.io/api/today");
+                publishProgress(75);
+                GetQuoteFromJson(result);
+                publishProgress(100);
+                return "true";
+            }
+            catch(Exception e){
+                return null;
+            }
+        }
+
+        private InputStream downloadUrl(String urlString) throws IOException {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(1000000 /* milliseconds */);
+            conn.setConnectTimeout(1500000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            return conn.getInputStream();
+        }
+
+        private void GetQuoteFromJson(InputStream input){
+            try {
+                // Feed InputStream into string, possibly replace with IOUtils.toString if allowed
+                BufferedReader inputReader = new BufferedReader(new InputStreamReader(input));
+                StringBuilder jsonString = new StringBuilder();
+                String line = inputReader.readLine();
+                while (line != null) {
+                    jsonString.append(line + "\n");
+                    line = inputReader.readLine();
+                }
+                // API sends a json array despite only having one element, so just read the 0th element
+                JSONArray jsonArray = new JSONArray(jsonString.toString());
+                JSONObject quoteObj = jsonArray.getJSONObject(0);
+                quote = quoteObj.getString("q") + "\n-" + quoteObj.getString("a");
+            }
+            catch (Exception e){
+                // Input is mangled in some way
+                quote = "ERROR: Corrupted Input";
+                return;
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_quote_of_the_day);
+        ProgressBar bar = findViewById(R.id.progressBar);
         Calendar calendar = Calendar.getInstance();
+        bar.setVisibility(View.VISIBLE);
         day = calendar.get(Calendar.DAY_OF_WEEK);
-        Button button = findViewById(R.id.change_day);
-        drawQuote();
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                day++;
-                if(day == 8)
-                    day = 1;
-                drawQuote();
-            }
-        });
+        ResolveDayImage();
+        bar.setProgress(50);
+        QuoteQuery query = new QuoteQuery(this);
+        query.execute();
     }
-
-    public void drawQuote(){
-        TextView quote = findViewById(R.id.quote);
+    private void ResolveDayImage(){
         ImageView quoteBackground = findViewById(R.id.quote_background);
         switch(day){
             case Calendar.SUNDAY:
-                quote.setText(R.string.sunday_quote);
-                quoteBackground.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.qotd1));
+                quoteBackground.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.qotd1));
                 break;
             case Calendar.MONDAY:
-                quote.setText(R.string.monday_quote);
-                quoteBackground.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.qotd2));
+                quoteBackground.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.qotd2));
                 break;
             case Calendar.TUESDAY:
-                quote.setText(R.string.tuesday_quote);
-                quoteBackground.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.qotd3));
+                quoteBackground.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.qotd3));
                 break;
             case Calendar.WEDNESDAY:
-                quote.setText(R.string.wednesday_quote);
-                quoteBackground.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.qotd4));
+                quoteBackground.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.qotd4));
                 break;
             case Calendar.THURSDAY:
-                quote.setText(R.string.thursday_quote);
-                quoteBackground.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.qotd5));
+                quoteBackground.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.qotd5));
                 break;
             case Calendar.FRIDAY:
-                quote.setText(R.string.friday_quote);
-                quoteBackground.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.qotd6));
+                quoteBackground.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.qotd6));
                 break;
             case Calendar.SATURDAY:
-                quote.setText(R.string.saturday_quote);
-                quoteBackground.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.qotd7));
+                quoteBackground.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.qotd7));
                 break;
         }
     }
