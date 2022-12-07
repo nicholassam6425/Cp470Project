6cac205c94c66caeb69e884db8813e035f26a228
mehansri <srit1420@mylaurier.ca>
2022-12-06 20:44:17 -0500

    v3

--- app/src/main/java/com/example/cp470project/StressRelief.java
+++ app/src/main/java/com/example/cp470project/StressRelief.java
@@ -3,6 +3,7 @@ package com.example.cp470project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -14,6 +15,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -21,7 +23,6 @@ public class StressRelief extends AppCompatActivity {
 public static final long starttimeinms = 5999;
 private TextView countdown;
 private TextView p;
-
 private Button bpause;
 private Button breset;
 private CountDownTimer countdowntime;
@@ -30,9 +31,6 @@ private long leftinmls = starttimeinms;
 private int i =0;
 int seconds;
 ImageView icanchor;
-Animation rounding;
-Animation rounding2;
-Animation rounding3;
 ObjectAnimator anim;
 
     @Override
@@ -43,30 +41,15 @@ ObjectAnimator anim;
         bpause = findViewById(R.id.button );
         breset =findViewById(R.id.button2);
         icanchor =findViewById(R.id.icanchor);
-
-//    rounding= AnimationUtils.loadAnimation(this,R.anim.rounding);
-//    rounding2= AnimationUtils.loadAnimation(this,R.anim.rounding);
-//    rounding3 = AnimationUtils.loadAnimation(this, R.anim.rounding);
         anim = ObjectAnimator.ofFloat(icanchor, "rotation", 0, 360);
-
-
-//        icanchor.setRotation(30);
         anim.setDuration(5999);
         anim.setRepeatCount(5);
         anim.setRepeatMode(ObjectAnimator.RESTART);
-
         p = findViewById(R.id.textView2);
         String ss = String.format("Inhale",(leftinmls/ 1000)%60);
         p.setText(ss);
 
-
-//
-//        int color = Color.parseColor("black"); //The color u want
-//        icanchor.setColorFilter(color);
-
         bpause.setOnClickListener(new View.OnClickListener() {
-
-
             public void onClick(View view) {
 
                 if (mrunning){
@@ -80,23 +63,16 @@ ObjectAnimator anim;
                 else{
                     anim.start();
                 }
-
             }
             }
-
         });
 
         breset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
                 resetimer();
             }
         });
-     // updatecountdowntext();
-
     }
 
     private void Startimer(){
@@ -105,81 +81,46 @@ ObjectAnimator anim;
             @Override
             public void onTick(long l) {
                 leftinmls =l;
-                Log.d("time:", ""+leftinmls);
-
                 updatecountdowntext();
             }
 
             @Override
             public void onFinish() {
                 mrunning= false;
-                bpause.setText("Start");
+                bpause.setText(R.string.start);
                 bpause.setVisibility(View.INVISIBLE);
                 breset.setVisibility(View.VISIBLE);
-
-
-                Log.d("myTag", ""+i);
-
                     if (i == 0) {
                         resetimer();
-
-                        p.setText("Hold");
+                        p.setText(R.string.hold);
                         leftinmls=8000;
                         Startimer();
-
-//                        rounding2.setDuration(8000);
-//                        icanchor.startAnimation(rounding2);
-//                        anim.setCurrentFraction(0.185f);
-
                         anim.setDuration(8000);
                         anim.start();
-
                     }
                     if (i == 1) {
                         resetimer();
-                        p.setText("Exhale");
+                        p.setText(R.string.exhale);
                         leftinmls=9000;
-
                         Startimer();
-//                        rounding3.setDuration(9000);
-//                        icanchor.startAnimation(rounding3);
                         anim.setDuration(9000);
                         anim.start();
 
                     }
-//                if (i == 2) {
-//                    Log.d("22222222222", ""+i);
-//                    p.setText("breath in for 5 seconds");
-//                    leftinmls=6000;
-//
-//                    Startimer();
-//                }
-
-
-                    else{
-
-                    }
-
-
-
                 i += 1;
 
             }
         }.start();
         mrunning= true;
-        bpause.setText("Pause");
+        bpause.setText(R.string.pause);
         breset.setVisibility(View.INVISIBLE);
         updatecountdowntext();
-//        icanchor.startAnimation(rounding);
-//        anim.start();
-
     }
     private void pausetimer(){
         countdowntime.cancel();
         mrunning =  false;
-        bpause.setText("Start");
+        bpause.setText(R.string.start);
         breset.setVisibility(View.VISIBLE);
-
        anim.pause();
     }
 
@@ -188,28 +129,27 @@ ObjectAnimator anim;
         leftinmls = starttimeinms;
         breset.setVisibility(View.INVISIBLE);
         bpause.setVisibility(View.VISIBLE);
-        p.setText("Inhale");
+        p.setText(R.string.inhale);
         anim.end();
         countdown.setText("00:00");
         if(i>=3){
             i=0;
         }
-
-
     }
 
     private void updatecountdowntext(){
         int minutes =  (int)(leftinmls/ 1000)/60;
          seconds =  (int)(leftinmls/ 1000)%60;
 
-        Log.d("seconds time when reset:", ""+seconds);
-
         String  timeleftformatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
         if (timeleftformatted.contains("00:00")&&(i==2)){
             anim.end();
             anim.setDuration(5990);
-
-
+            Context context = getApplicationContext();
+            CharSequence text = "Reset to run again";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
         countdown.setText(timeleftformatted);
     }
